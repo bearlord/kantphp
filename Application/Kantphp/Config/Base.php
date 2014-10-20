f5d4eda48f9c7ae29f80e64138893e5093a09b01
zhangsan <zhagnsan6947008@126.com>
2014-10-20 17:40:32 +0800

    第二个版本

---
+++ Application/Kantphp/Config/Base.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+    'module_type' => false,
+    'path_info_repair' => false,
+    'route' => array(
+        'module' => 'index',
+        'ctrl' => 'index',
+        'act' => 'index',
+        'data' => array(
+            'GET' => array()
+        )
+    ),
+    'debug' => false,
+    'url_suffix' => 'html',
+);
+
